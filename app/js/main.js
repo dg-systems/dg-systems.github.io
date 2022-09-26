4956b83ea9004068c73ec78dbaa1011228761bf1
Вадим Лосенков <114349649+dg-systems@users.noreply.github.com>
2022-09-26 22:27:10 +0300

    added fullpage scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
+import './plugins/fullpage.js';
+
 import { isWebp } from './utills.js';
 isWebp();
+
+const pageSlider = new fullpage('#fullpage', {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+});
