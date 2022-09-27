1348787bd5144acf792cc86e2ed7580727bad63e
Вадим Лосенков <114349649+dg-systems@users.noreply.github.com>
2022-09-27 22:33:25 +0300

    added product block and navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+import Swiper, { Controller, Navigation } from 'swiper';
 import './plugins/fullpage.js';
 
 import { isWebp } from './utills.js';
@@ -8,3 +9,27 @@ const pageSlider = new fullpage('#fullpage', {
   autoScrolling: true,
   scrollHorizontally: true,
 });
+
+const productInfoSlider = new Swiper('.product__info-slider', {
+  direction: 'vertical',
+  watchSlidesProgress: true,
+  modules: [Controller, Navigation],
+  controller: {
+    inverse: true,
+  },
+  navigation: {
+    nextEl: '.product-slider-next',
+    prevEl: '.product-slider-prev',
+  },
+});
+const productItemsSlider = new Swiper('.product__items-slider', {
+  direction: 'vertical',
+  watchSlidesProgress: true,
+  modules: [Controller],
+  controller: {
+    inverse: true,
+  },
+});
+
+productInfoSlider.controller.control = productItemsSlider;
+productItemsSlider.controller.control = productInfoSlider;
